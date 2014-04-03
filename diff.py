2cd30c262f04a70cb9ce2e07e8d7f84395d96c4c
Howon Lee <howon@howonlee.com>
2014-04-03 10:30:40 -0700

    first iteration of the repo again

---
+++ diff.py
@@ -0,0 +1,19 @@
+import csv, json
+
+howon = csv.DictReader(open("howon.csv"))
+roberto = csv.DictReader(open("roberto.csv"))
+titles1 = set()
+titles2 = set()
+for row in howon:
+	titles1.add(row["Title"])
+for row in roberto:
+	titles2.add(row["Title"])
+print "in 1, but not in 2: "
+print "=================="
+print json.dumps(sorted(list(titles1 - titles2)), sort_keys=True, indent=4, separators=(',', ': ')) # in 1, not in 2
+print "in 2, but not in 1: "
+print "=================="
+print json.dumps(sorted(list(titles2 - titles1)), sort_keys=True, indent=4, separators=(',', ': ')) # in 2, not in 1
+print "in both:"
+print "=================="
+print json.dumps(sorted(list(titles1 & titles2)), sort_keys=True, indent=4, separators=(',', ': ')) # in both
